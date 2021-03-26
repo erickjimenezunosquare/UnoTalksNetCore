7a514ec12f52c9f084cc216744dbd0a1667d7b54
Erick Unosquare <erick.jimenez@unosquare.com>
2021-03-25 21:20:10 -0600

    PUT BOOK INFO

---
+++ UnoTalksNetCore/CoreFramework/BookSOLID/EmptyClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace CoreFramework.BookSOLID
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+            /*
+            
+            Adaptive Code via C#: Agile coding with design patterns and SOLID principles
+
+            Gary McLean Hall
+            
+            PUBLISHED BYMicrosoft PressCopyright © 2014 by Gary McLean Hall. All rights reserved. 
+            No part of the contents of this book may be reproduced or transmitted in any form or by any means without the written permission of the publisher. 
+            Library of Congress Control Number: 2014943458 ISBN: 978-0-7356-8320-4
+
+            */
+        }
+    }
+}
