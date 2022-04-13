c07a3ffdec5c5d6cc99d2acc1f7445dd0022f54a
Erick Jimenez <Erick.Jimenez@coxautoinc.com>
2022-04-13 09:53:02 -0500

    Added missing csproj file, working again with updated nugets

--- UnoTalksNetCore/SeleniumFramework/Extras/SampleClass.cs
+++ UnoTalksNetCore/SeleniumFramework/Extras/SampleClass.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Threading;
+﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Threading;
 
 namespace Framework
 {
@@ -16,7 +16,7 @@ namespace Framework
 
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl("https://google.com");
-            driver.FindElementByName("q").SendKeys("Erick Eduardo Jimenez Rodriguez");
+            driver.FindElement(By.Name("q")).SendKeys("Erick Eduardo Jimenez Rodriguez");
 
             Thread.Sleep(5000);
 
